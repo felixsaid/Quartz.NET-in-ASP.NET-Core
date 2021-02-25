84456a860af9d8f3df6c351f3f7cb800af8b60d4
Felix Kiamba <kiambafelix@tutanota.com>
2021-02-25 08:39:33 +0300

    Initial Commit

---
+++ Services/NotificationJob.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Logging;
+using Quartz;
+using System.Threading.Tasks;
+
+namespace CloneJobs.Services
+{
+    [DisallowConcurrentExecution]
+    public class NotificationJob : IJob
+    {
+        private readonly ILogger<NotificationJob> _logger;
+        public NotificationJob(ILogger<NotificationJob> logger)
+        {
+            _logger = logger;
+        }
+        public Task Execute(IJobExecutionContext context)
+        {
+            _logger.LogInformation("Hello world!");
+            return Task.CompletedTask;
+        }
+    }
+}
